4c7a92322f4c7c9ecbd1c8993393c8157d236d4d
andre_zktt <andrezicatti@gmail.com>
2025-09-12 15:12:29 -0300

    :lock: security: implement authentication methods to verify and hash passwords and also create an access token

---
+++ app/auth.py
@@ -0,0 +1,23 @@
+from passlib.context import CryptContext
+from datetime import datetime, timedelta, UTC
+from jose import JWTError, jwt
+from decouple import config
+
+SECRET_KEY = config("SECRET_KEY")
+ALGORITHM = config("ALGORITHM")
+ACCESS_TOKEN_EXPIRE_MINUTES = int(config("ACCESS_TOKEN_EXPIRE_MINUTES"))
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+def verify_password(plain_password, hashed_password):
+    return pwd_context.verify(plain_password, hashed_password)
+
+def get_password_hash(password):
+    return pwd_context.hash(password)
+
+def create_access_token(data: dict):
+    to_encode = data.copy()
+    expire = datetime.now(UTC) + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, ALGORITHM)
+    return encoded_jwt
