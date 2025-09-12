ab1b51ba9bad7dde61e136f70f160e63d080e721
andre_zktt <andrezicatti@gmail.com>
2025-09-12 15:17:29 -0300

    :sparkles: feat: create router for transactions related endpoints

---
+++ app/routers/transactions.py
@@ -0,0 +1,26 @@
+from fastapi import APIRouter, Depends
+from sqlalchemy.orm import Session
+from typing import List
+from .. import crud, schemas, models
+from ..database import get_database
+from ..dependencies import get_current_user
+
+router = APIRouter(
+    prefix="/transactions",
+    tags=["transactions"],
+    dependencies=[Depends(get_current_user)]
+)
+
+@router.post("/", response_model=schemas.Transaction)
+def create_transaction(transaction: schemas.TransactionCreate,
+                       db: Session = Depends(get_database),
+                       current_user: models.User = Depends(get_current_user)):
+    return crud.create_user_transaction(db=db, transaction=transaction, user_id=current_user.id)
+
+@router.get("/", response_model=List[schemas.Transaction])
+def read_transactions(skip: int = 0,
+                      limit: int = 100,
+                      db: Session = Depends(get_database),
+                      current_user: models.User = Depends(get_current_user)):
+    transactions = crud.get_transactions(db=db, user_id=current_user.id, skip=skip, limit=limit)
+    return transactions
